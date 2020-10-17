4f803f2641ec33005b84688fd79d5c5c646f1e66
Combii <davidstovle@gmail.com>
2020-10-17 14:31:18 +0200

    Almost working but exception "EncryptionEngine"

--- src/chatapplication_server/components/ClientSocketEngine/ClientEngine.java
+++ src/chatapplication_server/components/ClientSocketEngine/ClientEngine.java
@@ -8,6 +8,7 @@ package chatapplication_server.components.ClientSocketEngine;
 import SocketActionMessages.ChatMessage;
 import chatapplication_server.ComponentManager;
 import chatapplication_server.components.ConfigManager;
+import chatapplication_server.components.EncryptionEngine;
 import chatapplication_server.components.KeyStorage;
 import chatapplication_server.components.PrimeNumberGen;
 import chatapplication_server.components.PrimitiveRootGen;
@@ -28,224 +29,216 @@ import java.util.Scanner;
  *
  * @author atgianne
  */
-public class ClientEngine extends GenericThreadedComponent 
-{
-    KeyStorage  keyStorage = new KeyStorage(4);
-    //Integer secretKey = 4;
+public class ClientEngine extends GenericThreadedComponent {
+    KeyStorage keyStorage = new KeyStorage(4);
+    // Integer secretKey = 4;
 
-     /** Instance of the ConfigManager component */
+    /** Instance of the ConfigManager component */
     ConfigManager configManager;
-    
+
     /** Object for printing the secure socket server configuration properties */
     ServerStatistics lotusStat;
-    
+
     /** Flag indicating whether the Socket Server is running.... */
     boolean isRunning;
-    
+
     /** The Socket connection to the Chat Application Server */
     private Socket socket;
-    
-    /** Socket Stream reader/writer that will be used throughout the whole connection... */
+
+    /**
+     * Socket Stream reader/writer that will be used throughout the whole
+     * connection...
+     */
     private ObjectOutputStream socketWriter;
     private ObjectInputStream socketReader;
-    
+
     /**
      * Singleton instance of the SocketServerEngine component
      */
     private static ClientEngine componentInstance = null;
-    
+
     /**
      * Creates a new instance of SocketServerEngine
      */
     public ClientEngine() {
         isRunning = false;
     }
-    
+
     /**
-     * Make sure that we can only get one instance of the SocketServerEngine component.
-     * Implementation of the static getInstance() method.
+     * Make sure that we can only get one instance of the SocketServerEngine
+     * component. Implementation of the static getInstance() method.
      */
-    public static ClientEngine getInstance()
-    {
-        if ( componentInstance == null )
+    public static ClientEngine getInstance() {
+        if (componentInstance == null)
             componentInstance = new ClientEngine();
-        
+
         return componentInstance;
     }
-    
-     /**
-     * Implementation of IComponent.initialize method().
-     * This method is called upon initialize of the ClientEngine component and handles any configuration that needs to be
-     * done in the client before it connects to the Chat Application Server.
+
+    /**
+     * Implementation of IComponent.initialize method(). This method is called upon
+     * initialize of the ClientEngine component and handles any configuration that
+     * needs to be done in the client before it connects to the Chat Application
+     * Server.
      * 
      */
-    public void initialize() throws ComponentInitException
-    {
-
+    public void initialize() throws ComponentInitException {
 
         /** Get the running instance of the Configuration Manager component */
         configManager = ConfigManager.getInstance();
-                
+
         /** For printing the configuration properties of the secure socket server */
         lotusStat = new ServerStatistics();
-        
+
         /** Try and connect to the server... */
-        try
-        {
-            socket = new Socket( configManager.getValue( "Server.Address" ), configManager.getValueInt( "Server.PortNumber" ) );
-        }
-        catch ( Exception e )
-        {
-            display( "Error connecting to the server:" + e.getMessage() + "\n" );
+        try {
+            socket = new Socket(configManager.getValue("Server.Address"),
+                    configManager.getValueInt("Server.PortNumber"));
+        } catch (Exception e) {
+            display("Error connecting to the server:" + e.getMessage() + "\n");
             ClientSocketGUI.getInstance().loginFailed();
             return;
         }
-        
+
         /** Print that the connection was accepted */
-        display( "Connection accepted: " + socket.getInetAddress() + ":" + socket.getPort() + "\n" );
-        
+        display("Connection accepted: " + socket.getInetAddress() + ":" + socket.getPort() + "\n");
+
         /** Create the read/write object streams... */
-        try
-        {
+        try {
             /** Set up the stream reader/writer for this socket connection... */
-            socketWriter = new ObjectOutputStream( socket.getOutputStream() );
-            socketReader = new ObjectInputStream( socket.getInputStream() );
-            
+            socketWriter = new ObjectOutputStream(socket.getOutputStream());
+            socketReader = new ObjectInputStream(socket.getInputStream());
+
             /** Start the ListeFromServer thread... */
             new ListenFromServer().start();
-        }
-        catch ( IOException ioe )
-        {
-            display( "Exception creating new Input/Output Streams: " + ioe + "\n");
+        } catch (IOException ioe) {
+            display("Exception creating new Input/Output Streams: " + ioe + "\n");
             ComponentManager.getInstance().fatalException(ioe);
         }
-        
+
         /** Send our username to the server... */
-        try
-        {
-            socketWriter.writeObject( configManager.getValue( "Client.Username") );
+        try {
+            socketWriter.writeObject(configManager.getValue("Client.Username"));
             sendMessage(new ChatMessage(ChatMessage.PUBLICKEY, keyStorage.calcPublicKey() + ""));
-        }
-        catch ( IOException ioe )
-        {
-            display( "Exception during login: " + ioe );
+        } catch (IOException ioe) {
+            display("Exception during login: " + ioe);
             shutdown();
             ComponentManager.getInstance().fatalException(ioe);
         }
-        
+
         super.initialize();
     }
-    
+
     /**
      * Method for displaying a message in the Client GUI
      * 
      * @msg The string message to be displayed
      */
-    private void display( String msg )
-    {
-        ClientSocketGUI.getInstance().append( msg );
+    private void display(String msg) {
+        ClientSocketGUI.getInstance().append(msg);
     }
-    
+
     /**
      * Method for sending a message to the server
      * 
      * @param msg The message to be sent
      */
-    public void sendMessage( ChatMessage msg )
-    {
-        try 
-        {
-            socketWriter.writeObject(msg);
-        }
-        catch( IOException e ) 
-        {
+    public void sendMessage(ChatMessage msg) {
+        try {
+            if(msg.getType() == ChatMessage.MESSAGE){
+                String encryptedData = EncryptionEngine.encrypt(msg.getMessage(), keyStorage.getSharedSecret());
+                System.out.println("ENCRYPTEDDATA: " + encryptedData);
+                socketWriter.writeObject(encryptedData);
+            }
+            else{
+                socketWriter.writeObject(msg);
+            }
+        } catch (IOException e) {
             System.out.println("Aloha");
-            display( "Exception writing to server: " + e );
+            display("Exception writing to server: " + e);
         }
     }
-    
+
     /**
-     * Method holding the main logic of the Client Engine. It basically waits for inputs from the user to be sent to the Server.
+     * Method holding the main logic of the Client Engine. It basically waits for
+     * inputs from the user to be sent to the Server.
      */
-    public void componentMain()
-    {
-        while ( !mustShutdown )
-        {
+    public void componentMain() {
+        while (!mustShutdown) {
             /** Wait messages from the user... */
-            try
-            {
-                Thread.sleep( 7000 );
+            try {
+                Thread.sleep(7000);
+            } catch (InterruptedException ie) {
+
             }
-            catch ( InterruptedException ie )
-            {
-                
-            }
-            
+
             // read message from user
-            //String msg = scan.nextLine();
+            // String msg = scan.nextLine();
             String msg = ClientSocketGUI.getInstance().getPublicMsgToBeSent();
-            if ( msg.equals( "" ) )
+            if (msg.equals(""))
                 continue;
-                
+
             // logout if message is LOGOUT
-            if(msg.equalsIgnoreCase("LOGOUT")) {
-                    sendMessage(new ChatMessage(ChatMessage.LOGOUT, ""));
-                    // break to do the disconnect
-                    break;
+            if (msg.equalsIgnoreCase("LOGOUT")) {
+                sendMessage(new ChatMessage(ChatMessage.LOGOUT, ""));
+                // break to do the disconnect
+                break;
             }
             // message WhoIsIn
-            else if(msg.equalsIgnoreCase("WHOISIN")) {
-                    sendMessage(new ChatMessage(ChatMessage.WHOISIN, ""));				
-            }
-            else if (msg.equalsIgnoreCase("PRIVATEMESSAGE")){				// default to ordinary message
-                    sendMessage(new ChatMessage(ChatMessage.PRIVATEMESSAGE, msg));
-            }
-            else  {				// default to ordinary message
-                    sendMessage(new ChatMessage(ChatMessage.MESSAGE, msg));
+            else if (msg.equalsIgnoreCase("WHOISIN")) {
+                sendMessage(new ChatMessage(ChatMessage.WHOISIN, ""));
+            } else if (msg.equalsIgnoreCase("PRIVATEMESSAGE")) { // default to ordinary message
+                sendMessage(new ChatMessage(ChatMessage.PRIVATEMESSAGE, msg));
+            } else { // default to ordinary message
+                sendMessage(new ChatMessage(ChatMessage.MESSAGE, msg));
             }
         }
-        
+
         shutdown();
     }
-    
-    public ObjectInputStream getStreamReader()
-    {
+
+    public ObjectInputStream getStreamReader() {
         return socketReader;
     }
-    
-     /**
-     * Override GenericThreadedComponent.shutdown() method.
-     * Signal and wait until the ClientEngine thread, holding the secure socket connection, stops.
+
+    /**
+     * Override GenericThreadedComponent.shutdown() method. Signal and wait until
+     * the ClientEngine thread, holding the secure socket connection, stops.
      * 
      * 
      * @see GenericThreadedComponent
      */
-    public void shutdown() 
-    {
-         /** Close the secure socket server */
-        try
-        {
-            synchronized ( socket)
-            {                
+    public void shutdown() {
+        /** Close the secure socket server */
+        try {
+            synchronized (socket) {
                 /** Shut down the Client Socket */
                 socketReader.close();
                 socketWriter.close();
                 socket.close();
-                
+
                 isRunning = false;
-                
-                
-                /** Print in the Event area of the Server Windows GUI the close operation of the Socket Server... */
-                ClientSocketGUI.getInstance().append( "[CCEngine]:: Shutting down the Client Engine....COMPLETE (" + lotusStat.getCurrentDate() + ")\n" );
+
+                /**
+                 * Print in the Event area of the Server Windows GUI the close operation of the
+                 * Socket Server...
+                 */
+                ClientSocketGUI.getInstance().append("[CCEngine]:: Shutting down the Client Engine....COMPLETE ("
+                        + lotusStat.getCurrentDate() + ")\n");
             }
+        } catch (Exception e) {
+            /**
+             * Print to the logging stream that shutting down the Central System socket
+             * server failed
+             */
+            ClientSocketGUI.getInstance().append("[CCEngine]: Failed shutting down CS socket server -- "
+                    + e.getMessage() + " (" + lotusStat.getCurrentDate() + ")\n");
         }
-        catch ( Exception e )
-        {
-            /** Print to the logging stream that shutting down the Central System socket server failed */
-            ClientSocketGUI.getInstance().append("[CCEngine]: Failed shutting down CS socket server -- " + e.getMessage() + " (" + lotusStat.getCurrentDate() + ")\n");
-        }
-        
-        /** Invoke our parent's method to stop the thread running the secure socket server... */
+
+        /**
+         * Invoke our parent's method to stop the thread running the secure socket
+         * server...
+         */
         super.shutdown();
     }
 }
